e15cf83fe79d8ca9ebb381ea3a9507cb8ad39d48
Gjermund Østensvig <gjermuno@hiof.no>
2021-01-04 13:51:12 +0100

    removed list of hardcoded TA usernames

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
 # Add these fields to connect the bot to your guild / server.
 TOKEN = ''
 GUILD = ''
-
-# The TAs ant teacher usernames are hardcoded here
-# example: ['GjermundØ#7507', 'skarning#9497']
-ta_teacher_list = []
